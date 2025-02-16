8b9f90f0e2cb01ea6f1701a2f8f319c5f015ede7
AdnanFaseeh <adnanfaseeh2743@gmail.com>
2025-02-16 20:24:49 +0500

    update Name

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -49,21 +49,21 @@ export default function Home() {
     <motion.main variants={slideUp} initial="initial" animate="enter" className="relative flex h-screen overflow-hidden bg-white">
       <TrailEffect />
       <div className="absolute top-[30%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-        <h1 className="gradient-text">Welcome to Jazzy@ Portfolio</h1>
+        <h1 className="gradient-text">Welcome to D@ni Portfolio</h1>
       </div>
       <div className="absolute top-[calc(100vh-350px)]">
         <div ref={slider} className="relative whitespace-nowrap">
-          <p ref={firstText} className="gradient-text text-[230px] font-semibold pr-12">Freelance Jazzy@ -</p>
-          <p ref={secondText} className="gradient-text text-[230px] font-semibold pr-12 absolute left-full top-0">Freelance Jazzy@ -</p>
+          <p ref={firstText} className="gradient-text text-[230px] font-semibold pr-12">Freelance D@ni -</p>
+          <p ref={secondText} className="gradient-text text-[230px] font-semibold pr-12 absolute left-full top-0">Freelance D@ni -</p>
         </div>
       </div>
       <div data-scroll data-scroll-speed={0.1} className="absolute top-[48%] left-[70%] gradient-text text-2xl font-light">
-        <p className="m-0">Freelance</p>
-        <p className="m-0">2D Animation Expert</p>
+        <p className="m-0">Web development</p>
+        <p className="m-0">Web development Expert</p>
       </div>
       <div className="flex flex-col gap-2 gradient-text absolute right-[70%] top-[48%]">
         <h5 className="text-[24px] font-light">Freelance</h5>
-        <h5 className="text-[24px] font-light">Ms Office Expert</h5>
+        <h5 className="text-[24px] font-light">Wordpress  Expert</h5>
       </div>
     </motion.main>
   );
