4f78bacff4a8e78eb12afe892d1970bccb5f1f58
AdnanFaseeh <adnanfaseeh2743@gmail.com>
2025-02-17 11:22:34 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Jazzy Portfolio - 2D Animation & Excel Expert',
-  description: 'Showcasing creative 2D animations and Excel expertise',
+  title: 'D@ni Webdeveloper',
+  description: 'Showcasing creative Web design ',
   icons: {
     icon: '/favicon.png',
   },
